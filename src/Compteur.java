35b384fd9b79df208bbce597732ac3707d58c054
Ahmed DAOUD <ahmed_daoud@ens.univ-artois.fr>
2021-11-10 18:40:52 +0100

    Etape 1

---
+++ src/Compteur.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.Locale;
+import java.util.Scanner;
+
+public abstract class Compteur {
+    private String nomFichier;
+    private int nbMots;
+    private int nbMots5;
+    public abstract void addOccurrence(String mot);
+    public Compteur (String nomFichier){
+        try {
+        Scanner scanner=new Scanner(new File(nomFichier));
+        // On boucle sur chaque ligne
+        while (scanner.hasNextLine()) {
+            scanner.useLocale(Locale.US);
+            scanner.useDelimiter(",|\\-|\\.|\\!|\\(|\\)|\\'|\\\"|\t|\n|\r\n|\s");
+            String mot;
+            while (scanner.hasNext()){
+                mot=scanner.next();
+                mot.toLowerCase();
+                if (mot.length()>=5){
+                    addOccurrence(mot);
+                }
+            }
+        }
+        scanner.close();
+    }
+    catch (Exception e){
+        e.printStackTrace();
+        System.exit(-1);
+    }}
+}
---
+++ src/CompteurTest.java
@@ -0,0 +1,16 @@
+public class CompteurTest extends Compteur {
+
+    public CompteurTest(String fichierTexte) {
+        super(fichierTexte);
+    }
+
+    public void addOccurrence(String mot){
+        System.out.println(mot);
+    }
+    public static void main(String[] args){
+        if (
+                args.length<1){ System.err.println("nom de fichier manquant");}
+        else {
+            CompteurTest c = new CompteurTest(args[0]);}
+    }
+}
